a64112b82f84effc13d8a3ff61d46ba34417e31d
Isaac Ruiz <isaacruiz@Isaacs-MBP.lan>
2022-03-10 13:28:07 -0800

    updated models

--- Models/Department.js
+++ Models/Department.js
@@ -1,11 +1,14 @@
 const mongoose = require("../connection");
-const teamMemberSchema = require("./teamMemberSchema");
+const teamMemberSchema = require('./teamMemberSchema')
 
 const Schema = mongoose.Schema;
 
 const departmentSchema = new Schema({
-  dptName: {type: String, require:true},
-  member: [teamMemberSchema],
+  firstName: {type: String, require:true},  
+  lastName: String, 
+  email: String, 
+  phone: Number,
+  dptName: [teamMemberSchema]
 })
 
 module.exports = mongoose.model("Department", departmentSchema);
