02f527030230ea5f762df4c526a9b1a3f7b907c4
Kanya <sKCuA2@kua.local>
2022-03-10 15:09:47 -0800

    working on heroku deploy

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 let mongoURL = "";
 
 if (process.env.NODE_ENV === "production") {
-  mongoURL = "";
+  mongoURL = process.env.DB_URL;
 } else {
   mongoURL = "mongodb://127.0.0.1/back-end";
 }
