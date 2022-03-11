0d10b509ce823cc512e8cf200e85a14b62c1c73f
Kelechuku (KC) Anyalebechi <96217841+sabbathcoder@users.noreply.github.com>
2022-03-10 17:42:32 -0800

    Update Department.js
    Reverting and testing

--- Models/Department.js
+++ Models/Department.js
@@ -4,11 +4,7 @@ const teamMemberSchema = require('./teamMemberSchema')
 const Schema = mongoose.Schema;
 
 const departmentSchema = new Schema({
-  firstName: {type: String, require:true},  
-  lastName: String, 
-  email: String, 
-  phone: Number,
-  dptName: [teamMemberSchema]
+    dptName: String
 })
 
 module.exports = mongoose.model("Department", departmentSchema);
